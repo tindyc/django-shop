61e9936863f8ad7b2915f322f5acf4093f8dc0fa
Tindy Chan <tindychan@Tindys-MacBook-Pro.local>
2023-04-01 14:21:51 +0100

    Refactoring account templates

--- account/views.py
+++ account/views.py
@@ -18,7 +18,7 @@ from .tokens import account_activation_token
 def dashboard(request):
     orders = user_orders(request)
     return render(request,
-                  'account/user/dashboard.html',
+                  'account/dashboard/dashboard.html',
                   {'section': 'profile', 'orders': orders})
 
 
@@ -33,7 +33,7 @@ def edit_details(request):
         user_form = UserEditForm(instance=request.user)
 
     return render(request,
-                  'account/user/edit_details.html', {'user_form': user_form})
+                  'account/dashboard/edit_details.html', {'user_form': user_form})
 
 
 @login_required
@@ -67,7 +67,7 @@ def account_register(request):
                 'token': account_activation_token.make_token(user),
             })
             user.email_user(subject=subject, message=message)
-            return HttpResponse('registered succesfully and activation sent')
+            return render(request, 'account/registration/register_email_confirm.html', {'form': registerForm})
     else:
         registerForm = RegistrationForm()
     return render(request, 'account/registration/register.html', {'form': registerForm})
